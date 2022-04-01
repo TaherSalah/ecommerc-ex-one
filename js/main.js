0d8513e262db73f90100a9a8b186d3fb63e65f47
taher salah <tahersalah2016@gmail.com>
2022-04-01 03:10:05 +0200

    no commit now

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/////////////////////////////// Start cart section ////////////////////////////////////
+let productImg = document.querySelector('#productImg');
+let smallImg1 = document.querySelector('#small-img1');
+let smallImg2 = document.querySelector('#small-img2');
+let smallImg3 = document.querySelector('#small-img3');
+let smallImg4 = document.querySelector('#small-img4');
+
+function changeImgGallary1() {
+    let productImg = document.getElementById('productImg').src = 'images/gallery-1.jpg'
+
+
+}
+
+function changeImgGallary2() {
+    let productImg = document.getElementById('productImg').src = 'images/gallery-2.jpg'
+
+}
+
+function changeImgGallary3() {
+    let productImg = document.getElementById('productImg').src = 'images/gallery-3.jpg'
+
+}
+
+function changeImgGallary4() {
+    let productImg = document.getElementById('productImg').src = 'images/gallery-4.jpg'
+
+}
+////////////////////////////////// End Cart Section ////////////////////////////
